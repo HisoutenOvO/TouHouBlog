056f97d29f85b705bb38033b23e8b2d2f98939b7
shi-changyu123 <1163481801@qq.com>
2026-05-16 12:37:40 +0800

    初始化后端文件

---
+++ src/main/java/BlogBack/TouHouBlogApplication.java
@@ -0,0 +1,15 @@
+package BlogBack;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class TouHouBlogApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TouHouBlogApplication.class,args);
+        log.info("TouHouBlog项目启动成功");
+    }
+}

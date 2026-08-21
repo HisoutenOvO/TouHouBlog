4f61e1aaa4fe667a4ed103562f3ac2bf92b1db59
HisoutenOVO <1163481801@qq.com>
2026-08-21 17:50:41 +0800

    部署阶段遇到少数问题，解决后部署阶段基本完成，域名注册完毕后即可投入使用，难道说！！！

--- TouHouBlog-Frontend/astro.config.mjs
+++ TouHouBlog-Frontend/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   viewTransitions: true,
   integrations: [vue()],
-  // Vite 相关配置
   vite: {
     plugins: [tailwindcss()],
     server: {
